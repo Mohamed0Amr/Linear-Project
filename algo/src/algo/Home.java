82b599b14df7fcd80196e75aa73010915fb916d7
Mohamed0Amr <140004433+Mohamed0Amr@users.noreply.github.com>
2024-02-14 21:54:30 +0200

    Add files via upload

---
+++ algo/src/algo/Home.java
@@ -0,0 +1,171 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package algo;
+
+import com.sun.source.tree.BreakTree;
+import java.awt.ComponentOrientation;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Random;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author boica
+ */
+public class Home extends JFrame implements ActionListener {
+
+    JPanel m1, p2, p3, p4;
+    JLabel l1, l2, l4, l5;
+    JTextField tf1, tf2, tf3, tf4;
+    JButton b2, b1, b3;
+    int key;
+    int inte = 0;
+    
+    
+
+    ArrayList<String> numbers = new ArrayList<>();
+
+    public Home() {
+        m1 = new JPanel(new GridLayout(3, 1));
+        p2 = new JPanel(new FlowLayout());
+        l2 = new JLabel("the key");
+        tf2 = new JTextField();
+        tf2.setPreferredSize(new Dimension(100, 30));
+        p3 = new JPanel(new FlowLayout());
+        l4 = new JLabel("The array");
+        tf3 = new JTextField();
+        tf3.setPreferredSize(new Dimension(300, 50));
+        tf3.setEditable(false);
+
+        p4 = new JPanel(new FlowLayout());
+        b1 = new JButton("Next");
+        b2 = new JButton("Reset");
+        b1.setPreferredSize(new Dimension(250, 40));
+        b2.setPreferredSize(new Dimension(250, 40));
+        b3 = new JButton("Custome");
+        b3.setPreferredSize(new Dimension(200, 20));
+
+        tf4 = new JTextField();
+        tf4.setPreferredSize(new Dimension(300, 50));
+        tf4.setEditable(false);
+        l5 = new JLabel("                  ");
+
+        p3.add(l4);
+        p3.add(tf3);
+        p3.add(l5);
+        p3.add(tf4);
+        p2.add(p3);
+        p2.add(l2);
+        p2.add(tf2);
+        p2.add(b3);
+        m1.add(p3);
+        m1.add(p2);
+        p4.add(b1);
+        p4.add(b2);
+        m1.add(p4);
+
+        this.add(m1);
+        this.setVisible(true);
+        this.setSize(500, 500);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        defultarray();
+        b1.addActionListener(this);
+        b1.setActionCommand("n");
+        b2.addActionListener(this);
+        b2.setActionCommand("R");
+        b3.setActionCommand("c");
+        b3.addActionListener(this);
+        
+        this.setLocationRelativeTo(null);
+        
+
+    }
+
+    public void defultarray() {
+        String s1 = "";
+
+        for (int i = 0; i < 6; i++) {
+            Random rand = new Random();
+
+            int randomNum = rand.nextInt(100);
+            s1 += randomNum + "   ";
+            numbers.add(randomNum + "");
+        }
+        tf3.setText(s1);
+
+    }
+
+    public void Next() {
+
+        String key = tf2.getText();
+        String s2 = "";
+        if (inte != -1) {
+            if (inte <numbers.size()){
+
+            if (key.equals(numbers.get(inte))) {
+                System.out.println(" found");
+                s2 += numbers.get(inte) + "✓  ";
+                tf4.setText(tf4.getText() + s2);
+                inte = -1;
+
+            } else {
+                s2 += numbers.get(inte) + "✗ ";
+                tf4.setText(tf4.getText() + s2);
+                inte++;
+
+            }
+        }
+
+    }
+    }
+    public void restet (){
+         String s1 = "";
+         tf3.setText("");
+         tf4.setText("");
+         numbers.clear();
+
+        for (int i = 0; i < 6; i++) {
+            Random rand = new Random();
+
+            int randomNum = rand.nextInt(100);
+            s1 += randomNum + "   ";
+            numbers.add(randomNum + "");
+        }
+        tf3.setText(s1);
+        inte =0;
+        
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getActionCommand().equals("n")) {
+            Next();
+
+        }
+        else if (e.getActionCommand().equals("R")){
+            restet();
+            
+        }
+        else if (e.getActionCommand().equals("c")){
+            Custome c1 = new Custome();
+            this.setVisible(false);
+        }
+        
+        
+
+
+    }
+}
